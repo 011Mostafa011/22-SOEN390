562e359ff4895ef1800bde7128304c432f284aad
Ben Marcotte <ben.marcotte99@gmail.com>
2022-02-02 03:34:49 -0500

    signup works

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -17,7 +17,6 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Signup from './Component/signup';
 
 function App() {
 
