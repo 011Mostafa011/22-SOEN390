08c575a15e405e4d19a5f19973e857ec46cfdc63
Ben Marcotte <ben.marcotte99@gmail.com>
2022-02-01 23:30:32 -0500

    stuff

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,19 +15,28 @@ import {MDCTopAppBar} from '@material/top-app-bar'
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login'
 
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </div>
+      <header class="mdc-top-app-bar">
+        <div class="mdc-top-app-bar__row">
+          <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button" aria-label="Open navigation menu">menu</button>
+          </section>
+        <div/>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
 
       <Routes>
         <Route path="/" element={<Dashboard/>}>
